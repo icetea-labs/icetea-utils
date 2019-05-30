a645c53361fd14fd9c3d2c88ce8ebfa8cf1af4d6
Truong Hong Thi <thi1981@gmail.com>
2019-05-30 18:01:23 +0700

    Add expect, revert, validate

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const Big = require('big.js')
+const Joi = require('@hapi/joi')
 
 const TEA_DECIMAL = 6
 const TEA_TO_MICRO = 10 ** TEA_DECIMAL
@@ -9,4 +10,23 @@ exports.toUnit = function (tea) {
 
 exports.toTEA = function (unit) {
   return new Big(String(unit)).div(TEA_TO_MICRO).toString()
+}
+
+exports.revert = function (message) {
+  throw new Error(message || 'Transaction reverted.')
+}
+
+exports.expect = function (condition, message) {
+  if (!condition) {
+    exports.revert(message)
+  }
+}
+
+exports.validate = function (value, schema, options) {
+  const { value: validatedValue, error } = Joi.validate(value, schema, options)
+  if (error) {
+    throw error
+  }
+
+  return validatedValue
 }
