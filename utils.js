7c8e996f0728aebe6a129ef0ba029a4836ac09bc
Truong Hong Thi <thi1981@gmail.com>
2019-06-05 16:10:30 +0700

    Publish to npm

--- utils.js
+++ utils.js
@@ -4,11 +4,11 @@ const Joi = require('@hapi/joi')
 const TEA_DECIMAL = 6
 const TEA_TO_MICRO = 10 ** TEA_DECIMAL
 
-exports.toUnit = function (tea) {
+exports.toMicroUnit = function (tea) {
   return new Big(String(tea)).times(TEA_TO_MICRO).toFixed()
 }
 
-exports.toTEA = function (unit) {
+exports.toStandardUnit = function (unit) {
   return new Big(String(unit)).div(TEA_TO_MICRO).toString()
 }
 
